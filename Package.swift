37597789844e163302dbf8d2727d15a894de69e0
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-07-13 09:30:39 +0200

    Updated Package.Swift

--- Package.swift
+++ Package.swift
@@ -1,31 +1,58 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+//import PackageDescription
+//
+//let package = Package(
+//    name: "IndianFlagSmokeEffect",
+//    platforms: [
+//         .iOS(.v13)
+//     ],
+//    products: [
+//        // Products define the executables and libraries a package produces, and make them visible to other packages.
+//        .library(
+//            name: "IndianFlagSmokeEffect",
+//            targets: ["IndianFlagSmokeEffect"]),
+//    ],
+////    dependencies: [
+////        // Dependencies declare other packages that this package depends on.
+////        // .package(url: /* package url */, from: "1.0.0"),
+////    ],
+//    targets: [
+//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+//        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//        .target(
+//            name: "IndianFlagSmokeEffect",
+//            dependencies: []),
+//        .testTarget(
+//            name: "IndianFlagSmokeEffectTests",
+//            dependencies: ["IndianFlagSmokeEffect"]),
+//    ]
+//)
+
+
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "IndianFlagSmokeEffect",
     platforms: [
-         .iOS(.v13)
-     ],
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "IndianFlagSmokeEffect",
-            targets: ["IndianFlagSmokeEffect"]),
+            targets: ["IndianFlagSmokeEffect"]
+        ),
     ],
-//    dependencies: [
-//        // Dependencies declare other packages that this package depends on.
-//        // .package(url: /* package url */, from: "1.0.0"),
-//    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "IndianFlagSmokeEffect",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "IndianFlagSmokeEffectTests",
-            dependencies: ["IndianFlagSmokeEffect"]),
+            dependencies: ["IndianFlagSmokeEffect"]
+        ),
     ]
 )
